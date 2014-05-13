dcee206c5099b8b9f05440bf465843e9dd5936b0
lxc <lurma@qq.com>
2014-05-13 22:28:51 +0800

    完善无限级分类工具类

--- Admin/Class/Category.class.php
+++ Admin/Class/Category.class.php
@@ -2,6 +2,7 @@
 
 class Category {
 
+	// 组合一维数组
 	public static function unlimitForLevel($cates, $html = '--', $pid = 0, $level = 0) {
 		$arr = array();
 		foreach($cates as $val) {
@@ -12,33 +13,60 @@ class Category {
 				$arr = array_merge($arr, self::unlimitForLevel($cates, $html, $val['id'], $level + 1));
 			}
 		}
-		return $cates;
+		return $arr;
+	}
+
+	// 组合多维数组
+	public static function unlimitForLayer($cates, $pid = 0, $name = 'child') {
+		$arr = array();
+		foreach($cates as $val) {
+			if($val['pid'] == $pid) {
+				$val[$name] = self::unlimitForLayer($cates, $val['id'], $name);
+				$arr[] = $val;
+			}
+		}
+
+		return $arr;
+	}
+
+	// 传递子类ID，获取父类ID
+	public static function getParents($cates, $id) {
+		$arr = array();
+		foreach($cates as $val) {
+			if($val['id'] == $id) {
+				$arr[] = $val;
+				$arr = array_merge(self::getParents($cates, $val['pid']), $arr);
+			}
+		}
+
+		return $arr;
 	}
 
 
+	// 传递一个父级分类ID，返回所有子级分类
+	public static function getChildren($cates, $pid) {
+		$arr = array();
+		foreach($cates as $val) {
+			if($val['pid'] == $pid) {
+				$arr[] = $val;
+				$arr = array_merge($arr, self::getChildren($cates, $val['id']));
+			}
+		}
+		return $arr;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// 传递一个父级分类ID，返回所有子级分类ID
+	public static function getChildIds($cates, $pid) {
+		$arr = array();
+		foreach($cates as $val) {
+			if($val['pid'] == $pid) {
+				$arr[] = $val['id'];
+				$arr = array_merge($arr, self::getChildren($cates, $val['id']));
+			}
+		}
+		return $arr;
+	}
 
 }
-
-
-
 
 ?>
